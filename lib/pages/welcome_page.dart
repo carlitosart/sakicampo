03a3fbb3ff146dfddf2de5c6278e126e54311d20
Carlos Nazir Peralta Lima <peralta.lima.carlos@gmail.com>
2021-12-01 22:36:59 -0400

    Tengo Miedo x1

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sakil/misc/colors.dart';
 import 'package:sakil/widgets/app_large_text.dart';
 import 'package:sakil/widgets/app_text.dart';
+import 'package:sakil/widgets/responsive_button.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({Key? key}) : super(key: key);
@@ -26,32 +28,50 @@ class _WelcomePageState extends State<WelcomePage> {
               width: double.maxFinite,
               height: double.maxFinite,
               decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage(
-                    "img/"+images[index]
-                  ),
-                  fit: BoxFit.cover,
-                )
-              ),
+                  image: DecorationImage(
+                image: AssetImage("img/" + images[index]),
+                fit: BoxFit.cover,
+              )),
               child: Container(
-                margin: const EdgeInsets.only(top: 150,left: 20,right: 20),
+                margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         AppLargeText(text: "Hola Mundo"),
-                        AppText(text: "HoliBoli",size: 30),
+                        AppText(text: "HoliBoli", size: 30),
+                        SizedBox(height: 20,),
                         Container(
                           width: 250,
+                          child: AppText(
+                            text: "Lalalalalalalalalalalalalalalalalala",
+                            color: AppColors.textColor2,
+                            size: 14,
+                          ),
                         ),
+                        SizedBox(height: 20,),
+                        ResponsiveButton(width: 120,)
                       ],
+                    ),
+                    Column(
+                      children: List.generate(3, (indexDots) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 2),
+                          width: 8,
+                          height: index==indexDots?25:8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: index==indexDots?AppColors.mainColor:AppColors.mainColor.withOpacity(0.3),
+                          ),
+                        );
+                      }),
                     ),
                   ],
                 ),
               ),
             );
-
           }),
     );
   }
